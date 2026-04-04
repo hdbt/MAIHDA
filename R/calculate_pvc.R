93763ec3f1f2599967dd6ff076e4fe68d9b8a99a
Raighto <spamdeliciouseguard-68@yahoo.de>
2026-04-04 23:31:02 +0200

    Update testthat setup and add stats::as.formula import

--- R/calculate_pvc.R
+++ R/calculate_pvc.R
@@ -272,6 +272,7 @@ print.pvc_result <- function(x, ...) {
 #' strata_result <- make_strata(maihda_sim_data, c("gender", "race"))
 #' stepwise_pcv(strata_result$data, "health_outcome", c("gender", "race", "age"))
 #' }
+#' @importFrom stats as.formula
 #'
 #' @export
 stepwise_pcv <- function(data, outcome, vars, engine = "lme4", family = "gaussian") {
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,1 +1,12 @@
-shinytest2::test_app()
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do your testing?
+# - If you are developing a package, run your tests using covr::report(),
+#   devtools::test(), or by pressing Ctrl/Cmd + Shift + T.
+# - If you are testing a script, run your tests using testthat::test_file()
+
+library(testthat)
+library(MAIHDA)
+
+test_check("MAIHDA")
